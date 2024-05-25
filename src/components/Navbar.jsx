533062cc9df1e02ff76d0cc4b1da5c1ab08c02ad
unknown <fathur.risqo@gmail.com>
2024-05-25 22:05:01 +0700

    navbar implementation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import '../css/Navbar.css';
 import logo from '../../public/friskay.svg';
-import menu from '../../public/Hamburger-Menu.svg';
-
-function handleNav() {
-  console.log('nav clicked');
-}
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  function handleNav() {
+    setNav(!nav);
+    console.log('Aku kangen Xena');
+  }
+  const isNav = nav ? 'slide' : '';
   return (
     <nav>
-      <header>
+      <header className="nav-menu">
         <div className="Wrapper">
           <div className="Logo-Navigation">
             <div className="Logo-Wrapper">
@@ -18,8 +20,29 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          <div className="Menu" onClick={handleNav}>
-            <img src={menu} alt="menu" className="Hamburger-Menu" />
+          <div className="Menu">
+            <ul className={`${isNav}`}>
+              <li>
+                <a href="#">Our Food</a>
+              </li>
+              <li>
+                <a href="#">Ingredients</a>
+              </li>
+              <li>
+                <a href="#">Studies</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="Menu-toggle-wrapper">
+          <div className="Menu-toggle" onClick={handleNav}>
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
         </div>
       </header>
