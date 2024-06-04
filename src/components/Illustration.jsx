2879b12b40d97f0cd355a7f419c51c5252238284
apirahman55 <apirahman55@gmail.com>
2024-06-04 08:12:00 +0700

    refactor (illustration): making some example for responsiveness styling decoration

--- src/components/Illustration.jsx
+++ src/components/Illustration.jsx
@@ -4,7 +4,15 @@ import illustration from '../../public/Illustration.svg';
 const Illustration = () => {
   return (
     <section className="Image-Container">
-      <div className="Illustration-Section">
+      <div className='Illustration-Wrapper'>
+        <div className='Illustration-Decoration' />
+
+        <div className='Illustration-Image'>
+          <img className='Illustration-Svg' src={illustration} alt="Mwah" />
+        </div>
+      </div>
+      
+      {/* <div className="Illustration-Section">
         <div className="Illustration-Wrapper">
           <div className="Illustration">
             <div className="Illustration-Image">
@@ -12,7 +20,8 @@ const Illustration = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
+
     </section>
   );
 };
