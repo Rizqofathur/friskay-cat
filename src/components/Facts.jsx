51d2ed6c2ba6d5f50e75f14715712f997569baa0
unknown <fathur.risqo@gmail.com>
2024-06-17 16:54:02 +0700

    facts direfactor jadi pake fungsi map()

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -1,6 +1,25 @@
 import '../css/Facts.css';
 
 const Facts = () => {
+  const facts = [
+    {
+      id: 1,
+      caption: 'Free from artificial colors, flavors, and preservatives, our cat food offers a natural and nutritious diet for your feline friend.',
+    },
+    {
+      id: 2,
+      caption: 'Enriched with omega-3 and omega-6 fatty acids, it promotes a shiny coat and healthy skin, making it ideal for cats with sensitive skin.',
+    },
+    {
+      id: 3,
+      caption: 'Packed with high-quality protein, our new cat food supports strong muscles and a healthy heart in cats of all ages.',
+    },
+    {
+      id: 4,
+      caption: 'Formulated with prebiotics and probiotics, this cat food aids in digestion and supports a balanced gut microbiome.',
+    },
+  ];
+
   return (
     <section className="Facts-section">
       <div className="Facts-image">
@@ -8,30 +27,14 @@ const Facts = () => {
       </div>
       <div className="Facts-container">
         <div className="Facts-content">
-          <div className="Fact">
-            <div className="Fact-header">
-              <h4 className="Fact-header-text">FACT 1</h4>
+          {facts.map((fact) => (
+            <div className="Fact" key={fact.id}>
+              <div className="Fact-header">
+                <h4 className="Fact-header-text">FACT {fact.id}</h4>
+              </div>
+              <p className="Fact-caption">{fact.caption}</p>
             </div>
-            <p className="Fact-caption">Free from artificial colors, flavors, and preservatives, our cat food offers a natural and nutritious diet for your feline friend.</p>
-          </div>
-          <div className="Fact">
-            <div className="Fact-header">
-              <h4 className="Fact-header-text">FACT 2</h4>
-            </div>
-            <p className="Fact-caption">Enriched with omega-3 and omega-6 fatty acids, it promotes a shiny coat and healthy skin, making it ideal for cats with sensitive skin</p>
-          </div>
-          <div className="Fact">
-            <div className="Fact-header">
-              <h4 className="Fact-header-text">FACT 3</h4>
-            </div>
-            <p className="Fact-caption">Packed with high-quality protein, our new cat food supports strong muscles and a healthy heart in cats of all ages.</p>
-          </div>
-          <div className="Fact">
-            <div className="Fact-header">
-              <h4 className="Fact-header-text">FACT 4</h4>
-            </div>
-            <p className="Fact-caption">Formulated with prebiotics and probiotics, this cat food aids in digestion and supports a balanced gut microbiome.</p>
-          </div>
+          ))}
         </div>
       </div>
     </section>
