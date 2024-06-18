f0b7eaa4dc9b2b81b912d8101f93690a6a9da997
unknown <fathur.risqo@gmail.com>
2024-06-18 13:09:12 +0700

    Finish the project, YEAYY

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,7 @@
 import '../css/Card.css';
 import quote from '../../public/img/profile/quote.png';
-import profile1 from '../../public/img/profile/profile-1.jpg';
 
-const Card = () => {
+const Card = ({ profile, username, instance, review }) => {
   return (
     <div className="Card">
       <div className="Card-container">
@@ -10,17 +9,14 @@ const Card = () => {
           <img src={quote} alt="quote" className="quote-img" />
         </div>
         <div className="Card-inner">
-          <p className="Card-text-review">
-            I&apos;ve never seen a cat food like this before. The nutritional balance is exceptional, and I&apos;ve noticed remarkable improvements in the coat and energy levels of the cats I&apos;ve recommended it to. It&apos;s a
-            game-changer in feline nutrition.
-          </p>
+          <p className="Card-text-review">{review}</p>
           <div className="Card-profile">
             <div className="Card-profile-image">
-              <img className="Card-img" src={profile1} alt="profile-account" />
+              <img className="Card-img" src={profile} alt="profile-account" />
             </div>
             <div className="Card-profile-Identity">
-              <p className="Card-profile-name">Fathurrizqo</p>
-              <p className="Card-profile-instance">Informatika, UNNES</p>
+              <p className="Card-profile-name">{username}</p>
+              <p className="Card-profile-instance">{instance}</p>
             </div>
           </div>
         </div>
--- src/components/ReviewCards.jsx
+++ src/components/ReviewCards.jsx
@@ -1,14 +1,51 @@
 import '../css/ReviewCards.css';
 import Card from './Card';
+import profile1 from '../../public/img/profile/profile-1.jpg';
+import profile2 from '../../public/img/profile/profile-2.jpg';
+import profile3 from '../../public/img/profile/profile-3.jpg';
+import profile4 from '../../public/img/profile/profile-4.png';
 
 const ReviewCards = () => {
+  const reviews = [
+    {
+      id: 1,
+      profile: profile1,
+      username: 'Fathurrizqo',
+      instance: 'Informatika, UNNES',
+      caption:
+        "I've never seen a cat food like this before. The nutritional balance is exceptional, and I've noticed remarkable improvements in the coat and energy levels of the cats I've recommended it to. It's a game-changer in feline nutrition.",
+    },
+    {
+      id: 2,
+      profile: profile2,
+      username: 'Holyservant',
+      instance: 'Google',
+      caption:
+        "I've never seen a cat food like this before. The nutritional balance is exceptional, and I've noticed remarkable improvements in the coat and energy levels of the cats I've recommended it to. It's a game-changer in feline nutrition.",
+    },
+    {
+      id: 3,
+      profile: profile3,
+      username: 'Makima',
+      instance: 'ChainsawMan.inc',
+      caption:
+        "I've never seen a cat food like this before. The nutritional balance is exceptional, and I've noticed remarkable improvements in the coat and energy levels of the cats I've recommended it to. It's a game-changer in feline nutrition.",
+    },
+    {
+      id: 4,
+      profile: profile4,
+      username: 'David',
+      instance: 'Gadgetin',
+      caption: 'Halo guys, David disini. sebenernya saya gatau kenapa bisa ada di sini, yang pasti ulah developernya yang ga ada bahan buat bikin text lagi. bukan mau review hp atau gadget apapun, saya cuma mau ngisi konten buat card ini.',
+    },
+  ];
+
   return (
     <section className="review-cards">
       <div className="cards">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {reviews.map((review) => (
+          <Card profile={review.profile} instance={review.instance} username={review.username} review={review.caption} key={review.id} />
+        ))}
       </div>
     </section>
   );
